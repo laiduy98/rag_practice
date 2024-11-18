097c7d158441f9403ae50dd13a5ae66dd804fe82
Khang Duy LAI <laiduy98@gmail.com>
2024-11-18 15:56:19 +0100

    add some comments

--- movegpt_rag/config.py
+++ movegpt_rag/config.py
@@ -1,6 +1,7 @@
 import os
 from pydantic_settings import BaseSettings
 
+# Define constant here
 DATA_PATH = "./data/faqs.json"
 SIMILARITY_THRESHOLD = 0.8
 
@@ -9,5 +10,10 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
+
+
+# Check if env file is there
+if not os.path.exists(".env"):
+    raise FileNotFoundError("Environment file is missing.")
 
 settings = Settings()
